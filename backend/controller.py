05341a133fe466fb70a5a0ea3863882184c6fb43
drhollar <drhollar@ncsu.edu>
2019-09-27 11:58:37 -0400

    refactored json utils into its own files. refactored controller entry point into its own function

--- backend/controller.py
+++ backend/controller.py
@@ -3,33 +3,9 @@ import backend.preprocess.preprocess_main            as prp
 import backend.field_extraction.field_extractor_main as fe
 import backend.data_extraction.data_extraction_main  as de
 import backend.postprocess.postprocess_main          as pop
-
-from dataclasses_json import dataclass_json
-from typing import List
+import backend.json_utils.json_utils                 as ju
 
 import cv2
-
-
-"""
-Writes a list to a specified file in JSON format.
- 
-@param fields: list of fields of type FieldData
-@param filename: file to write to
-"""
-def createJSONFromFieldDataList(fields: List[fd.FieldData]):
-    json_str = ""
-    id = 0
-    for field in fields:
-        json_str += str("\"element" + str(id) + "\": " + field.to_json(indent=4) + ",\n")
-        id += 1
-    return json_str[:-2] + "\n"
-
-def writeToJSONFile(json_str, filename): 
-    fp = open(filename, 'w+')
-    fp.write("{\n")
-    fp.write(json_str)
-    fp.write("}\n")
-    fp.close()
     
 """
 Current entry point for the program.
@@ -43,10 +19,7 @@ Controls the overall pipeline:
 6. Write to JSON file
 7. Send postprocess data to the frontend 
 """
-def main():
-    # image_file = "resources/images/simple_check.jpg"
-    image_file = "resources/images/check_example.jpg"
-
+def controller_entry_point(image_file):
     # Read RGB image as greyscale
     img = cv2.imread(image_file)  
 
@@ -81,8 +54,14 @@ def main():
     ##################################################
     pop.postprocessEntryPoint(image, fields)
 
-    json_str = createJSONFromFieldDataList(fields)
-    writeToJSONFile(json_str, "out.json")
+    json_str = ju.createJSONFromFieldDataList(fields)
+    ju.writeToJSONFile(json_str, "out.json")
+
+def main():
+    # image_file = "resources/images/simple_check.jpg"
+    image_file = "resources/images/check_example.jpg"
+
+    controller_entry_point(image_file)
 
 if __name__ == "__main__":
     main()
