43c9892beffb0b2fddc1fa5dffcc2f8057774848
Taha <mtkarimi@ncsu.edu>
2019-10-02 11:36:32 -0400

    1. Added fields_api.py file and folder to backend 2. Added the out.json file that is output by the demo's controller.py as well (for testing). 3. Also added the get_fields api call from fields_api.py to main.py.

--- frontend/main.py
+++ frontend/main.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, send_file
 
 ALLOWED_EXTENSIONS = {'png', 'jpg', 'jpeg', 'bmp', 'pdf'}
 
@@ -8,6 +8,13 @@ app = Flask(__name__)
 @app.route("/")
 def home():
     return render_template("home.html")
+
+@app.route('/field-data', methods=['GET'])
+def get_fields():
+    try:
+        return send_file('../../out.json', attachment_filename='out.json')
+    except Exception as e:
+        return str(e)
 
 # Runs the program
 if __name__ == "__main__":
