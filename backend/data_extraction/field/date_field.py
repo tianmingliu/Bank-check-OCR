01a775ce023a02a02ef155f518596705e58a84a5
Tianming Liu <tliu18@ncsu.edu>
2019-11-07 17:03:15 -0500

    add account, date and memo validation

--- backend/data_extraction/field/date_field.py
+++ backend/data_extraction/field/date_field.py
@@ -1,5 +1,6 @@
 import backend.data_extraction.field.data.field_data as field_data
 import backend.data_extraction.field.field as field
+import datetime
 
 class DateField(field.Field):
     def identify(self, field_data: field_data.DataPair):
@@ -7,6 +8,19 @@ class DateField(field.Field):
 
     def validate(self, data: field_data.DataPair):
         print("Validating if the passed data is a valid date")
+
+        date = data.data_info.extractedData
+        day, month, year = date.split('/')
+
+        try:
+            datetime.datetime(int(year), int(month), int(day))
+        except ValueError:
+            print("The date is invalid.")
+            return False
+
+        print("The date is valid.")
+        return True
+
     
     def get_type(self):
         return field_data.FieldType.FIELD_TYPE_DATE
---
+++ backend/data_extraction/field/memo_field.py
@@ -0,0 +1,24 @@
+import backend.data_extraction.field.data.field_data as field_data
+import backend.data_extraction.field.field as field
+
+class MemoField(field.Field):
+    def identify(self, field: field_data.DataPair):
+        print("Identifying if the passed data is the memo number...")
+
+        if field_data.DataPair.data.field_type == field.field_data.FieldType.FIELD_TYPE_MEMO:
+            return True
+        else:
+            return False
+
+    def validate(self, data: field_data.DataPair):
+        print("Validating if the passed data is a valid memo number")
+
+        try:
+            str(data.data_info.extractedData)
+        except ValueError:
+            print("Memo is not a string")
+            return False
+
+
+    def get_type(self):
+        return field_data.FieldType.FIELD_TYPE_MEMO
