5367f5bb11d9d2177ee2107031214022259ea34a
Tianming Liu <tliu18@ncsu.edu>
2019-11-07 17:23:58 -0500

    add account, date and memo validation

--- backend/data_extraction/field/data/field_data.py
+++ backend/data_extraction/field/data/field_data.py
@@ -12,32 +12,33 @@ Represents a type of field on a check. There are 5 types:
 3. Data
 4. Amount
 5. Routing
-
 NOTE(Dustin): It might be worth splitting date into Written and 
 NonWritten
 """
-@dataclass_json
+# @dataclass_json
 class FieldType(Enum):
-    FIELD_TYPE_NONE      = 0
-    FIELD_TYPE_SIGNATURE = 1
-    FIELD_TYPE_DATE      = 2
-    FIELD_TYPE_AMOUNT    = 3
-    FIELD_TYPE_ROUTING   = 4
-    FIELD_TYPE_ACCOUNT   = 5
+    FIELD_TYPE_NONE            = 0
+    FIELD_TYPE_DATE            = 1
+    FIELD_TYPE_PAY_TO_ORDER_OF = 2
+    FIELD_TYPE_AMOUNT          = 3
+    FIELD_TYPE_AMOUNT_WRITTEN  = 4
+    FIELD_TYPE_SIGNATURE       = 5
+    FIELD_TYPE_MEMO            = 6
+    FIELD_TYPE_ROUTING         = 7
+    FIELD_TYPE_ACCOUNT         = 8
 
 """
 Represents a bounding box on an image. 
-
 @field x: start x coordinate of bounding rectangle
 @field y: start y coordinate of bounding rectangle
 @field w: width of the bounding rectangle
 @field h: height of the bounding rectangle
 """
-@dataclass_json
+# @dataclass_json
 @dataclass
 class BoundingRect:
     x: float = 0
-    y: float = 0 
+    y: float = 0
     w: float = 0
     h: float = 0
 
@@ -49,12 +50,11 @@ class BoundingRect:
 
 """
 Represents the actual data of a field on a check.
-
 @field extractedData: String representation of the data.
 @field confidence: how confident it is that the extracted
                    data is accurate
 """
-@dataclass_json
+# @dataclass_json
 @dataclass
 class FieldDataInfo:
     extracted_data: str   = ""
@@ -62,7 +62,6 @@ class FieldDataInfo:
 
 """
 Represents the data for a Field on a check. 
-
 @field field_type: The type of field represented for the instance.
 @field bounds: The bounding box for the field on an image of a check
 @field data_info: The information for the field that was extracted from
